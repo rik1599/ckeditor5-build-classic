c9596715435647baa52b3178b191284616fe53e6
Riccardo <belliatoriccardo@gmail.com>
2019-12-09 14:15:27 +0100

    first build

---
+++ src/ckeditor5-columnwidget/src/columnboxediting.js
@@ -0,0 +1,122 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+
+import { toWidget, toWidgetEditable } from '@ckeditor/ckeditor5-widget/src/utils';
+import Widget from '@ckeditor/ckeditor5-widget/src/widget';
+
+import InsertColumnBoxCommand from './insertcolumnboxcommand';
+
+export default class ColumnBoxEditing extends Plugin {
+    static get requires() {
+        return [ Widget ];
+    }
+
+    /**
+     * Init the plugin
+     */
+    init() {
+        this._defineSchema();
+        this._defineConverters();
+
+        this.editor.commands.add('insertColumnBox', new InsertColumnBoxCommand(this.editor));
+    }
+
+    /**
+     * Define the schema for the plugin
+     * <columnBox>
+     *      <columnBoxChild></columnBoxLeft>
+     *      <columnBoxChild></columnBoxRight>
+     * </columnBox>
+     */
+    _defineSchema() {
+        const schema = this.editor.model.schema;
+
+        schema.register('columnBox', {
+            isObject: true,
+            allowWhere: '$block'
+        });
+
+        schema.register('columnBoxChild', {
+            isLimit: true,
+            allowIn: 'columnBox',
+            allowContentOf: '$root'
+        });
+
+        schema.addChildCheck((context, childDefinition) => {
+            if (context.endsWith( 'columnBoxChild' ) && childDefinition.name == 'columnBox') {
+                return false;
+            }
+        });
+    }
+
+    /**
+     * Convert the schema of the object into its HTML rappresentation
+     * e.g.
+     * <columnBox>
+     *      <columnBoxChild></columnBoxLeft>
+     *      <columnBoxChild></columnBoxRight>
+     * </columnBox>
+     * 
+     * will become
+     * <section class="column-box">
+     *      <div class="column-box-child"></div>
+     *      <div class="column-box-child"></div>
+     * </section>
+     */
+    _defineConverters() {
+        const conversion = this.editor.conversion;
+
+        conversion.for('upcast').elementToElement({
+            model: 'columnBox',
+            view: {
+                name: 'section',
+                classes: 'column-box'
+            }
+        });
+
+        conversion.for('dataDowncast').elementToElement({
+            model: 'columnBox',
+            view: {
+                name: 'section',
+                classes: 'column-box'
+            }
+        });
+
+        conversion.for('editingDowncast').elementToElement({
+            model: 'columnBox',
+            view: (modelElement, viewVriter) => {
+                const section = viewVriter.createContainerElement('section', {class: 'column-box'});
+
+                return toWidget(section, viewVriter, {
+                    label: 'column box widget', 
+                    hasSelectionHandle: true
+                });
+            }
+        });
+        
+
+        conversion.for('upcast').elementToElement({
+            model: 'columnBoxChild',
+            view: {
+                name: 'div',
+                classes: 'column-box-child'
+            }
+        });
+
+        conversion.for('dataDowncast').elementToElement({
+            model: 'columnBoxChild',
+            view: {
+                name: 'div',
+                classes: 'column-box-child'
+            }
+        });
+
+        conversion.for('editingDowncast').elementToElement({
+            model: 'columnBoxChild',
+            view: (modelElement, viewVriter) => {
+                const div = viewVriter.createEditableElement('div', {class: 'column-box-child'});
+
+                return toWidgetEditable(div, viewVriter);
+            }
+        });
+    }
+}
---
+++ src/ckeditor5-columnwidget/src/insertcolumnboxcommand.js
@@ -0,0 +1,39 @@
+import Command from '@ckeditor/ckeditor5-core/src/command';
+
+export default class InsertColumnBoxCommand extends Command {
+    /**
+     * It is called when 'insertColumnBox' is called
+     */
+    execute() {
+        this.editor.model.change(writer => {
+            this.editor.model.insertContent( createColumnBox(writer) );
+        });
+    }
+
+    refresh() {
+        const model = this.editor.model;
+        const selection = model.document.selection;
+        const allowedIn = model.schema.findAllowedParent(selection.getFirstPosition(), 'columnBox');
+
+        this.isEnabled = allowedIn !== null;
+    }
+}
+
+/**
+ * Creates a columnbox and appends to it two columnBoxChilds
+ * @param writer the CKEditor writer
+ * @returns a new columnBox ready to be appended to the text
+ */
+function createColumnBox(writer) {
+    const columnBox = writer.createElement('columnBox');
+    const columnBoxLeft = writer.createElement('columnBoxChild');
+    const columnBoxRight = writer.createElement('columnBoxChild');
+
+    writer.append(columnBoxLeft, columnBox);
+    writer.append(columnBoxRight, columnBox);
+
+    writer.appendElement('paragraph', columnBoxLeft);
+    writer.appendElement('paragraph', columnBoxRight);
+
+    return columnBox;
+}
